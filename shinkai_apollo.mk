1c951e6b0695e7bedc93ba8b2466c96d0ca30e6c
GanX <daffaofficial224@gmail.com>
2026-09-02 17:01:46 +0700

    Update and rename lineage_apollo.mk to shinkai_apollo.mk

--- shinkai_apollo.mk
+++ shinkai_apollo.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, device/sony/apollo/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/shinkai/config/common_full_phone.mk)
 
 # Setup keystore
 -include vendor/lineage-priv/keys/keys.mk
@@ -28,3 +28,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-sony
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="H8324-user 10 52.1.A.3.49 052001A003004902006556692 release-keys" \
     BuildFingerprint=Sony/H8324/H8324:10/52.1.A.3.49/052001A003004902006556692:user/release-keys
+
+SHINKAI_MAINTAINER := Ganendra1945
