a9fdc6bbcf76789fb7bf14fef928ec9b3ea50c98
GanX <daffaofficial224@gmail.com>
2026-09-09 21:35:34 +0700

    Update product makefiles and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollo.mk
+    $(LOCAL_DIR)/matrixx_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollo-user \
-    lineage_apollo-userdebug \
-    lineage_apollo-eng
+    matrixx_apollo-user \
+    matrixx_apollo-userdebug \
+    matrixx_apollo-eng
