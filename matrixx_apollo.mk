6dcb8807882769793ba02fbd87ecceb463cd8a15
GanX <daffaofficial224@gmail.com>
2026-09-09 21:34:33 +0700

    Update device configuration for matrixx_apollo

--- matrixx_apollo.mk
+++ matrixx_apollo.mk
@@ -8,12 +8,12 @@
 $(call inherit-product, device/sony/apollo/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/matrixx/config/common_full_phone.mk)
 
 # Setup keystore
 -include vendor/lineage-priv/keys/keys.mk
 
-PRODUCT_NAME := lineage_apollo
+PRODUCT_NAME := matrixx_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_BRAND := Sony
@@ -28,3 +28,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-sony
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="H8324-user 10 52.1.A.3.49 052001A003004902006556692 release-keys" \
     BuildFingerprint=Sony/H8324/H8324:10/52.1.A.3.49/052001A003004902006556692:user/release-keys
+
+MATRIXX_MAINTAINER := Ganendra1945
